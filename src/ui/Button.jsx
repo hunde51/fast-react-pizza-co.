877c200b278e4745f83f3d4d26ec353e76805ce2
hunde51 <hundetesfa6@gmail.com>
2025-12-09 23:54:41 +0300

    feat: enhance UI components with Tailwind CSS, update Button styles, and improve layout

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,13 +1,19 @@
 import { Link } from "react-router-dom"
 import LinkButton from "./LinkButton"
 
-function Button({ children, disabled, to }) {
-    const className = "bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2   disabled:curser-not-allowed sm:px-6 sm:py-4";
+function Button({ children, disabled, to,type }) {
 
-    if (to) return <Link to={to} className={className}>{children}</Link>
+    const base = "bg-yellow-400 uppercase font-semibold text-stone-800  inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2   disabled:curser-not-allowed "
+    
+    const styles = {
+        
+        primary: base + '  py-3 px-4 md:px-6 md :py-4',
+        small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs'
+    }; 
+    if (to) return <Link to={to} className={styles[type]}>{children}</Link>
     
     return (
-        <div disabled={disabled}  className={className}>
+        <div disabled={disabled}  className={styles[type]}>
             {children}
         </div>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
+//eslint-disable-next-line
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      // custom font family
+      sans: ["Roboto Mono", "monospace"],
+    },
+    // when WE want override everything in here
+    extend: {
+      // here to extend or updating
+      fontSize: {
+        // huge custom text size
+        huge: ["8rem", { lineHeight: "1" }],
+      },
+      height: {
+        // fix mobile viewport height
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
