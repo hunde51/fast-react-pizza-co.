5daf3884a75050478544e635275a90ba1ab22839
hunde51 <hundetesfa6@gmail.com>
2025-12-11 20:05:38 +0300

    refactor components for improved readability and consistency, enhance styling with Tailwind CSS

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -1,20 +1,21 @@
-import { Link,  useNavigate } from "react-router-dom"
+import { Link, useNavigate } from 'react-router-dom';
 
 function LinkButton({ children, to }) {
-    const className = 'text-sm text-blue-600 hover:text-blue-700 hover:underline';
+  const navigate = useNavigate();
+  const className = 'text-sm text-blue-500 hover:text-blue-600 hover:underline';
 
-    const navigate = useNavigate();
-    
-    if (to === "-1") return <button className={className} onClick={() => navigate(-1)}>
-        {children}
-    </button>
-
-    
+  if (to === '-1')
     return (
-        <Link to={to} className={className}>{children}</Link>
-    )
+      <button className={className} onClick={() => navigate(-1)}>
+        {children}
+      </button>
+    );
 
-    
+  return (
+    <Link to={to} className={className}>
+      {children}
+    </Link>
+  );
 }
 
-export default LinkButton
+export default LinkButton;
