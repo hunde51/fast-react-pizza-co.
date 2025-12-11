325bbf1e514d85edbe40cb114eeabec5ac10fbd7
hunde51 <hundetesfa6@gmail.com>
2025-12-11 16:45:49 +0300

    feat: enhance Cart and CartItem components with improved styling and structure, add Delete button functionality

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,12 +3,13 @@ import LinkButton from "./LinkButton"
 
 function Button({ children, disabled, to,type }) {
 
-    const base = "bg-yellow-400 uppercase font-semibold text-stone-800  inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2   disabled:curser-not-allowed "
+    const base = "bg-yellow-400 text-sm uppercase font-semibold text-stone-800  inline-block rounded-full hover:bg-yellow-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:bg-yellow-300 focus:ring-offset-2   disabled:curser-not-allowed "
     
     const styles = {
         
         primary: base + '  py-3 px-4 md:px-6 md :py-4',
-        small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs'
+        small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs',
+        secondary: " text-sm uppercase border-2 border-stone-300 font-semibold text-stone-400  inline-block rounded-full hover:bg-stone-300   hover:text-stone-800 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-200 focus:text-stone-800 focus:bg-stone-300 focus:ring-offset-2   disabled:curser-not-allowed  py-2.5 px-4 md:px-6 md :py-3.5"
     }; 
     if (to) return <Link to={to} className={styles[type]}>{children}</Link>
     
@@ -17,6 +18,7 @@ function Button({ children, disabled, to,type }) {
             {children}
         </div>
     )
+    
 }
 
 export default Button
