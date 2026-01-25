3203c0341c2508a7835d60d20f220b681f918991
blen <hundetesfa6@gmail.com>
2026-01-25 14:09:03 +0300

    empty cart all cart changes of adding qunatity of pizza and delelteing  from the cart

--- src/features/cart/EmptyCart.jsx
+++ src/features/cart/EmptyCart.jsx
@@ -1,18 +1,15 @@
-import { Link } from 'react-router-dom';
 import LinkButton from '../../ui/LinkButton';
 
 function EmptyCart() {
   return (
-    <div className='py-3 px-4'>
-         <LinkButton to='/menu'>
-        &larr; Back to menu
-      </LinkButton>
+    <div className="px-4 py-3">
+      <LinkButton to="/menu">&larr; Back to menu</LinkButton>
 
-      <p className='font-semibold mt-7'>Your cart is still empty. Start adding some pizzas :)</p>
-    </div> 
+      <p className="mt-7 font-semibold">
+        Your cart is still empty. Start adding some pizzas :)
+      </p>
+    </div>
   );
 }
 
 export default EmptyCart;
-
-
