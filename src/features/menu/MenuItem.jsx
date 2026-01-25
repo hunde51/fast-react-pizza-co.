52242811431521fae1c9d34a7ae702f87b43fd47
blen <hundetesfa6@gmail.com>
2026-01-25 14:13:38 +0300

    correcting the names Deleting Items Create oredr and menus cha nges

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,12 +1,13 @@
-import { formatCurrency } from "../../utils/helpers";
+import { useDispatch, useSelector } from 'react-redux';
 import Button from '../../ui/Button';
-import { useDispatch, useSelector } from "react-redux";
-import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
-import DeleteItem from "../cart/DeleteItem";
-import UpdateItemQuantity from "../cart/UpdateItemQuantity";
+import DeleteItem from '../cart/DeleteItem';
+import UpdateItemQuantity from '../cart/UpdateItemQuantity';
+import { formatCurrency } from '../../utils/helpers';
+import { addItem, getCurrentQuantityById } from '../cart/cartSlice';
 
 function MenuItem({ pizza }) {
   const dispatch = useDispatch();
+
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   const currentQuantity = useSelector(getCurrentQuantityById(id));
@@ -28,16 +29,13 @@ function MenuItem({ pizza }) {
       <img
         src={imageUrl}
         alt={name}
-        className={`h-24 ${soldOut ? "opacity-70" : ""}`}
+        className={`h-24 ${soldOut ? 'opacity-70 grayscale' : ''}`}
       />
-
-      <div className="flex flex-col grow pt-0.5">
+      <div className="flex grow flex-col pt-0.5">
         <p className="font-medium">{name}</p>
-
-        <p className="text-sm italic text-stone-500 capitalize">
-          {ingredients.join(", ")}
+        <p className="text-sm capitalize italic text-stone-500">
+          {ingredients.join(', ')}
         </p>
-
         <div className="mt-auto flex items-center justify-between">
           {!soldOut ? (
             <p className="text-sm">{formatCurrency(unitPrice)}</p>
@@ -48,7 +46,7 @@ function MenuItem({ pizza }) {
           )}
 
           {isInCart && (
-            <div className='flex items-center gap-3 sm:gap-8'>
+            <div className="flex items-center gap-3 sm:gap-8">
               <UpdateItemQuantity
                 pizzaId={id}
                 currentQuantity={currentQuantity}
