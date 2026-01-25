3db23f3db7784d1e94b9f39b1ea72fb445f5086d
blen <hundetesfa6@gmail.com>
2026-01-25 14:15:04 +0300

    UI  all chnge in home order and updateitem quanitty

--- src/features/cart/UpdateItemQuantity.jsx
+++ src/features/cart/UpdateItemQuantity.jsx
@@ -1,18 +1,27 @@
-import { useDispatch } from "react-redux"
-import Button from "../../ui/Button"
-import { decreaseItemQuantity, increaseItemQuality } from "./cartSlice";
+import { useDispatch } from 'react-redux';
+import Button from '../../ui/Button';
+import { decreaseItemQuantity, increaseItemQuantity } from './cartSlice';
 
-function UpdateItemQuantity({pizzaId,currentQuantity}) {
-    const dispatch = useDispatch();
-    return (
-        <div className="flex gap-2 item-center md:gap-3 items-center">
-            <Button type="round" onClick={() => dispatch(decreaseItemQuantity(pizzaId))}>-</Button>
+function UpdateItemQuantity({ pizzaId, currentQuantity }) {
+  const dispatch = useDispatch();
 
-            <span className='text-sm font-medium'>{currentQuantity}</span>
-            
-            <Button type="round" onClick={()=>dispatch(increaseItemQuality(pizzaId))}>+</Button>
-        </div>
-    )
+  return (
+    <div className="flex items-center gap-2 md:gap-3">
+      <Button
+        type="round"
+        onClick={() => dispatch(decreaseItemQuantity(pizzaId))}
+      >
+        -
+      </Button>
+      <span className="text-sm font-medium">{currentQuantity}</span>
+      <Button
+        type="round"
+        onClick={() => dispatch(increaseItemQuantity(pizzaId))}
+      >
+        +
+      </Button>
+    </div>
+  );
 }
 
-export default UpdateItemQuantity
+export default UpdateItemQuantity;
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -3,8 +3,8 @@ import CreateUser from '../features/user/CreateUser';
 import Button from './Button';
 
 function Home() {
+  const username = useSelector((state) => state.user.username);
 
-  const username = useSelector((state) => state.user.username)
   return (
     <div className="my-10 px-4 text-center sm:my-16">
       <h1 className="mb-8  text-xl font-semibold md:text-3xl">
@@ -15,8 +15,12 @@ function Home() {
         </span>
       </h1>
 
-      {username === '' ? (<CreateUser />) : (
-        <Button to='/menu' type="primary">Continue Ordering,{username}</Button>
+      {username === '' ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          Continue ordering, {username}
+        </Button>
       )}
     </div>
   );
